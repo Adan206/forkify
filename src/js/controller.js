799d5e1939267c8c6fbc7a417f0fd845f3786e49
Adan Abbi <adan@Adans-MacBook-Air-2.local>
2023-03-10 20:30:47 -0800

    fetch

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const getRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+
+    if (!response.ok) throw Error(`${data.message} ${res.status}`);
+
+    console.log(response, data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getRecipe();
